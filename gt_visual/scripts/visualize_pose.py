afc97f866867554966fef7e2d7f6a5131823509d
김경현 <132433953+kyeonghyeon0314@users.noreply.github.com>
2024-07-16 20:33:25 +0900

    재제작한 GT에 맞게 수정

--- gt_visual/scripts/visualize_pose.py
+++ gt_visual/scripts/visualize_pose.py
@@ -3,7 +3,7 @@
 import rospy
 import tf
 import tf.transformations as tft
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped , PoseArray, Pose
 import time
 
 def parse_gt_file(file_path):
@@ -19,32 +19,32 @@ def parse_gt_file(file_path):
             poses.append((timestamp, (px, py, pz, qx, qy, qz, qw)))
     return poses
 
-def publish_pose(publisher, timestamp, pose):
-    pose_msg = PoseStamped()
-    pose_msg.header.stamp = rospy.Time.from_sec(timestamp)
-    pose_msg.header.frame_id = 'map'
-    pose_msg.pose.position.x = pose[0]
-    pose_msg.pose.position.y = pose[1]
-    pose_msg.pose.position.z = pose[2]
-    pose_msg.pose.orientation.x = pose[3]
-    pose_msg.pose.orientation.y = pose[4]
-    pose_msg.pose.orientation.z = pose[5]
-    pose_msg.pose.orientation.w = pose[6]
-    publisher.publish(pose_msg)
+def publish_pose(publisher, pose):
+    pose_array_msg = PoseArray()
+    pose_array_msg.header.frame_id = 'map'
+    for timestamp, pose in poses:
+        pose_msg = Pose()
+        pose_msg.pose.position.x = pose[0]
+        pose_msg.pose.position.y = pose[1]
+        pose_msg.pose.position.z = pose[2]
+        pose_msg.pose.orientation.x = pose[3]
+        pose_msg.pose.orientation.y = pose[4]
+        pose_msg.pose.orientation.z = pose[5]
+        pose_msg.pose.orientation.w = pose[6]
+        pose_array_msg.poses.append(pose_msg)
+    publisher.publish(pose_array_msg)
 
 def main():
     rospy.init_node('gt_pose_visualizer', anonymous=True)
-    pose_publisher = rospy.Publisher('/gt_pose', PoseStamped, queue_size=10)
+    pose_publisher = rospy.Publisher('/gt_poses', PoseStamped, queue_size=10)
 
-    gt_file_path = '/home/kimkh/PoseNet-Pytorch/PoseNet/AirLAB/pose_data_test.txt'  # 실제 파일 경로로 수정
+    gt_file_path = '/home/kimkh/PoseNet-Pytorch/PoseNet/AirLAB/pose_data_test.txt'  # 실제 파일 경로
     poses = parse_gt_file(gt_file_path)
 
     rate = rospy.Rate(1)  # 1 Hz
 
-    for timestamp, pose in poses:
-        if rospy.is_shutdown():
-            break
-        publish_pose(pose_publisher, timestamp, pose)
+    while not rospy.is_shutdown():
+        publish_pose(pose_publisher, poses)
         rate.sleep()
 
 if __name__ == '__main__':
