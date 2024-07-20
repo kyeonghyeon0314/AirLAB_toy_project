85b495c181a284c86cda7e095fdfb13f1037e257
김경현 <132433953+kyeonghyeon0314@users.noreply.github.com>
2024-07-20 15:54:06 +0900

    축 회전 및 축변환

--- gt_visual/scripts/visualize_pose.py
+++ gt_visual/scripts/visualize_pose.py
@@ -5,6 +5,7 @@ import tf
 import tf.transformations as tft
 from geometry_msgs.msg import PoseStamped , PoseArray, Pose
 import time
+import numpy as np
 
 def parse_gt_file(file_path):
     poses = []
@@ -23,13 +24,19 @@ def transform_pose(pose):
     px, py, pz = pose[0], pose[1], pose[2]
     qx, qy, qz, qw = pose[3], pose[4], pose[5], pose[6]
     
-    transformed_px = pz
-    transformed_py = px
+    transformed_px = -pz
+    transformed_py = -px
     transformed_pz = py
     
     quat = [qx, qy, qz, qw]
-    rpy = tft.euler_from_quaternion_from_euler(quat)
-    transformed_quat = tft.quaternion_from_euler(rpy[0], rpy[2], rpy[1])
+    #x 축 90도 회전
+    rot_quat_x = tft.quaternion_from_euler(np.pi / 2, 0, 0)
+    #z 축 -90도 회전
+    rot_quat_z = tft.quaternion_from_euler(0, 0, -np.pi / 2)
+
+    # 기존 퀀터니언과 회전 퀀터니언을 곱하여 새로운 퀀터니언 생성
+    quat_after_x = tft.quaternion_multiply(rot_quat_x, quat)
+    transformed_quat = tft.quaternion_multiply(rot_quat_z, quat_after_x)
     
     return transformed_px, transformed_py, transformed_pz, transformed_quat[0], transformed_quat[1], transformed_quat[2], transformed_quat[3]
 
@@ -52,7 +59,7 @@ def main():
     gt_file_path = '/home/kimkh/PoseNet-Pytorch/PoseNet/AirLAB/pose_data_test.txt'  # 실제 파일 경로
     poses = parse_gt_file(gt_file_path)
 
-    rate = rospy.Rate(1)  # 1 Hz
+    rate = rospy.Rate(60)  # 60 Hz
     pose_array_msg = PoseArray()
     pose_array_msg.header.frame_id = 'map'
 
